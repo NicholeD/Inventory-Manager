053250e765ae2b51563692232ab7b5a0bd9ca1cf
NicholeD <YOUREMAIL@YOUREMAIL.com>
2022-08-08 14:19:30 -0600

    Implemented BalancedCurlyBraceValidator::check() and :checkExtension. Tests pass.

--- GroupWork/KenzieJavaCompiler/src/main/java/com/kenzie/groupwork/kenziejavacompiler/BalancedCurlyBraceValidator.java
+++ GroupWork/KenzieJavaCompiler/src/main/java/com/kenzie/groupwork/kenziejavacompiler/BalancedCurlyBraceValidator.java
@@ -1,6 +1,8 @@
 package com.kenzie.groupwork.kenziejavacompiler;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Stack;
 
 /**
@@ -22,6 +24,28 @@ public class BalancedCurlyBraceValidator {
      */
     public boolean check(List<Character> fileCharacters) {
         // TODO: complete this method
+        Queue<Character> q = new LinkedList<>();
+        int count = 0;
+
+        for (Character c : fileCharacters) {
+            q.add(c);
+        }
+
+        while (!q.isEmpty()) {
+            char c = q.remove();
+            if (c == OPEN) {
+                count++;
+            } else if (c == CLOSE) {
+                count--;
+                if (count < 0) {
+                    return false;
+                }
+            }
+        }
+
+        if (count == 0) {
+            return true;
+        }
         return false;
     }
 
@@ -32,9 +56,48 @@ public class BalancedCurlyBraceValidator {
      * @return true if the braces are balanced, false otherwise
      */
     public boolean checkExtension(List<Character> fileCharacters) {
-        // TODO: complete this method
-        return false;
+        //keeping track of open braces
+        Stack<Character> openStack = new Stack<>();
+
+        String longestString = "";
+        String currentString = "";
+        int extraClose = 0;
+        int extraOpen = openStack.size();
+
+        //loop thru fileCharacters
+        for (Character c : fileCharacters) {
+            //checking if a close bracket comes before an unpaired open bracket
+            if (c == CLOSE && openStack.isEmpty()) {
+                extraClose++;
+            } else if (c == CLOSE) {
+                //matching an open bracket with a close bracket
+                openStack.pop();
+                //checking for the longest string
+                if (currentString.length() > longestString.length()) {
+                    longestString = currentString;
+                }
+                //resetting current string
+                currentString = "";
+            } else if (c == OPEN) {
+                openStack.push(c);
+
+            }  else if (!openStack.isEmpty()) {
+                currentString = currentString + c;
+            }
+        }
+
+        System.out.println("Total number of unbalanced brackets is " + (extraClose + extraOpen) +
+                "\nTotal unbalanced open brackets: " + extraOpen +
+                "\nTotal unbalanced close brackets: " + extraClose +
+                "\nLongest string of characters between a pair of balanced brackets: " + longestString);
+
+        if (extraClose == 0 && openStack.size() == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
+
 
     /**
      * Use this to enable or disable additional debug messages.
